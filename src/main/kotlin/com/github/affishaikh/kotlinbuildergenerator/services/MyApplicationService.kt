e548aa7c14f76856c3d7e694af4c421cb840a948
GitHub Action <action@github.com>
2020-07-25 13:28:51 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/affishaikh/kotlinbuildergenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.affishaikh.kotlinbuildergenerator.services
+
+import com.github.affishaikh.kotlinbuildergenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/affishaikh/kotlinbuildergenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/affishaikh/kotlinbuildergenerator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.affishaikh.kotlinbuildergenerator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.affishaikh.kotlinbuildergenerator.MyBundle
 
 class MyProjectService(project: Project) {
 
